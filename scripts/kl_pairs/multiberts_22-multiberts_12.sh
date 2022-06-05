8a220e12c376d9079bb20a72a52cef6780b6b63b
Michael Wilson <michael.a.wilson@yale.edu>
2022-06-04 20:34:57 -0400

    update time for jobs

--- scripts/kl_pairs/multiberts_22-multiberts_12.sh
+++ scripts/kl_pairs/multiberts_22-multiberts_12.sh
@@ -5,7 +5,7 @@
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=16G
-#SBATCH --time=48:00:00
+#SBATCH --time=01:30:00
 #SBATCH --gpus=v100:1
 #SBATCH --partition=gpu
 #SBATCH --mail-type=END,FAIL,INVALID_DEPEND
